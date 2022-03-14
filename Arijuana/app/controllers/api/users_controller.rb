cfdeebf5d2d97c2732b62a4b6b5976dfd736df49
AritonS <aritonsefedini@icloud.com>
2022-03-14 10:02:40 -0400

    Started user auth

--- Arijuana/app/controllers/api/users_controller.rb
+++ Arijuana/app/controllers/api/users_controller.rb
@@ -1,2 +1,32 @@
 class Api::UsersController < ApplicationController
+    before_action :require_logged_in, only: [:show]
+
+    def new
+        @user = User.new
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = User.find_by(params[:id])
+        render 'api/users/show'
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
--- Arijuana/app/models/user.rb
+++ Arijuana/app/models/user.rb
@@ -1,3 +1,42 @@
 class User < ApplicationRecord
+    before_validation :ensure_session_token
+
+    validates :username, :email, :password_digest, :session_token, presence: true
+    validates :username, :email, :session_token, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password
     
+    def passord=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    def generate_session_token
+        Securerandom::base64
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
 end
