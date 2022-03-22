7a50d305f7611b4ba8f15823a1e7132feb15b2db
AritonS <aritonsefedini@icloud.com>
2022-03-22 11:41:03 -0400

    Update App.jsx

--- Arijuana/frontend/components/App.jsx
+++ Arijuana/frontend/components/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SignupContainer from './session/signup_container';
 import SplashContainer from './splash/splash_container';
 
-const App = () => (
-    <div>
-        <h1>Arijuana</h1>
-        <SplashContainer />
-    </div>
-);
+const App = () => {
+    return (
+        <div>
+            <h1 id='app-name'>Arijuana</h1>
+            <Switch>
+                <Route path='/' component={SplashContainer}/>
+                <Route path='/signup' component={SignupContainer}/>
+                <Route path='/login' component={LoginContainer}/>
+            </Switch>
+        </div>
+    )
+};
 
 export default App;
