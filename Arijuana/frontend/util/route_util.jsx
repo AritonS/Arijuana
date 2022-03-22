149aa65fb6983e1388564e1845e44af1ec13aeed
AritonS <aritonsefedini@icloud.com>
2022-03-22 13:54:43 -0400

    Bootstrapping current user

---
+++ Arijuana/frontend/util/route_util.jsx
@@ -0,0 +1,29 @@
+import { connect } from "react-redux";
+import { Redirect, Route, withRouter } from 'react-router-dom';
+import React from 'react';
+
+const mSTP = state => ({
+    loggedIn: Boolean(state.session.currentUser)
+})
+
+// <AuthRoute path='' component={} />
+const Auth = ({loggedIn, path, component: Component}) => (
+    <Route 
+    path={path}
+    render={props => (
+        loggedIn ? <Redirect to={`/user/${currentUser.id}`} /> : <Component {...props} />
+    )}
+    />
+);
+
+const Protected = ({loggedIn, path, component: Component}) => (
+    <Route 
+    path={path}
+    render={props => (
+        loggedIn ? <Component {...props} /> : <Redirect to='/signup' />
+    )}
+    />
+);
+
+export const AuthRoute = withRouter(connect(mSTP)(Auth))
+export const ProtectedRoute = withRouter(connect(mSTP)(Protected))
