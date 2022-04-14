50923595b4a41b92fc5ad1ac9ad863db24ee1106
AritonS <aritonsefedini@icloud.com>
2022-04-14 08:55:29 -0400

    seed dispenaries

--- Arijuana/config/routes.rb
+++ Arijuana/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {formate: :json} do
-    
+    resources :users
+    resources :dispensaries
   end
 
   root 'static_pages#root'
--- Arijuana/db/seeds.rb
+++ Arijuana/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dispenary1 = Dispensary.create([{name: 'RISE'}, {latitude: 40.745534}, {longitude: -73.985899}])
+dispenary2 = Dispensary.create([{name: 'Cannabist'}, {latitude: 40.692943}, {longitude: -73.991026}])
+dispenary3 = Dispensary.create([{name: 'Potion'}, {latitude: 40.721783}, {longitude: -73.990285}])
